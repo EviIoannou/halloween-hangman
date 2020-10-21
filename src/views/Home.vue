71285ed2145d6ef623d81e013d3b5fff24803bd8
Markus Wiland <markus.wiland@outlook.com>
2020-10-21 14:37:16 +0200

    working on design

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,12 +3,14 @@
     {{/* BEM */}}
 
     <h1>HangMan</h1>
-    <input type="text" />
-    <input type="text" />
-    <button>Starta spel</button>
+    <div class="home__start">
+      <input type="text" />
+      <input type="text" />
+      <button>Starta spel</button>
+    </div>
   </div>
 </template>
-
+'fix'
 <script>
 export default {
   name: 'Home',
@@ -18,8 +20,22 @@ export default {
 
 <style scoped>
 .home {
-  height: 100%;
-  width: 100%;
-  background-color: red;
+  color: white;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+}
+.home > h1 {
+  text-align: center;
+}
+.home__start {
+  display: flex;
+  margin: auto;
+  flex-direction: column;
+}
+.home__start > input {
+  width: 200px;
+}
+.home__start > button {
 }
 </style>
