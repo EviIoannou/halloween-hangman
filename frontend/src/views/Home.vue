3481d67e3fbbf2e5fd86e05d3e456d59fb51d14b
AnionaStella <eiljon88@gmail.com>
2020-10-22 09:26:01 +0200

    Refactored into frontend and backend

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -2,17 +2,21 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <WordLength msg="3"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import WordLength from "@/components/HelloWorld.vue";
+
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    HelloWorld,
+    WordLength
   }
 };
 </script>
