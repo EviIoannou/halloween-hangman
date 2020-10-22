731734d821c6d755c7269d66ed4ff82950ab5ff5
AnionaStella <eiljon88@gmail.com>
2020-10-22 14:16:27 +0200

    view amount of letters in word

--- frontend/src/components/WordLength.vue
+++ frontend/src/components/WordLength.vue
@@ -1,16 +1,39 @@
 <template>
-
-    <h1>{{amount}}</h1>
-
+<div v-if="word">
+    <table>
+        <tr>
+            <!-- plats att fylla i bokstäver för framtida spelet -->
+            <!-- <td :key="letter" v-for="letter in word">{{letter}}</td> -->
+        </tr>
+        <tr>
+        <td :key="letter" v-for="letter in word">_</td>
+        </tr>
+    </table>
+    </div>
 </template>
 
 <script>
-
-
 export default {
+    created() {
+        fetch("http://localhost:3000/word")
+        .then(response => response.json())
+        .then(result => {
+            this.word = result;
+            // console.log(result);
+        });
+    },
+    data() {
+        return {
+        word: [],
+        };
+    },
   name: "WordLength",
-  props: {
-    msg: Number
-  }
 };
 </script>
+
+<style scoped>
+    td{
+        padding-left: 10px;
+        font-size: xx-large;
+    }
+</style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -2,14 +2,14 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
-    <WordLength msg="3"/>
+    <WordLength amount="3"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
-import WordLength from "@/components/HelloWorld.vue";
+import WordLength from "@/components/WordLength.vue";
 
 
 export default {
