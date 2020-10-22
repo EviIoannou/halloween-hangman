b0873710a51a35a4e7d8f560db5b6a17c08a75d3
Markus Wiland <markus.wiland@outlook.com>
2020-10-22 09:39:10 +0200

    fixed the design

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,32 @@
 <template>
   <div class="home">
     {{/* BEM */}}
+    <div class="home__content">
+      <h1>
+        HalloWeen HaNgMaN!1!1!
+      </h1>
+      <div class="home__inputs">
+        <label for="fname">Spelare Ett</label>
+        <input
+          type="text"
+          id="fname"
+          name="firstname"
+          placeholder="Spelare ett"
+        />
 
-    <h1>HangMan</h1>
-    <div class="home__start">
-      <label for="fname">First name:</label>
-      <input type="text" id="fname" name="fname" />
-      <label for="lname">Last name:</label>
-      <input type="text" id="lname" name="lname" />
-      <button>Starta spel</button>
+        <label for="lname">Spelare Två</label>
+        <input
+          type="text"
+          id="lname"
+          name="lastname"
+          placeholder="Spelare två"
+        />
+      </div>
+      <button class="home__button">Button</button>
     </div>
   </div>
 </template>
-'fix'
+
 <script>
 export default {
   name: 'Home',
@@ -22,25 +36,33 @@ export default {
 
 <style scoped>
 .home {
-  color: white;
-  width: 100%;
+  display: flex;
+  flex-direction: column;
   height: 100%;
+  justify-content: center;
+  align-items: center;
+}
+
+.home__content {
+  color: white;
+  margin-bottom: 200px;
+}
+.home__content > h1 {
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  height: 200px;
+}
+.home__inputs {
   display: flex;
   justify-content: center;
   flex-direction: column;
 }
-.home > h1 {
-  text-align: center;
-  margin-top: 10%;
+.home__inputs > input {
+  padding: 10px;
+  margin: 10px 0px;
 }
-.home__start {
-  display: flex;
-  flex-direction: column;
-  margin: auto;
-}
-.home__start > input {
-  width: 200px;
-}
-.home__start > button {
+.home__button {
+  padding: 10px;
 }
 </style>
