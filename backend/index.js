ec468d6b921ae39cfa49bdb8783bd11fa1d78234
Markus Wiland <markus.wiland@outlook.com>
2020-11-02 11:01:24 +0100

    added in backend

--- backend/index.js
+++ backend/index.js
@@ -17,9 +17,8 @@ app.get('/guess', (req,res) => {
 })
 
 app.get('/word', (req,res) => {
-    let dictionary = ["katt", "banan", "fotboll"];
     // choose word 
-    let chosenWord = dictionary[Math.floor(Math.random() * dictionary.length)];
+    let chosenWord = finalWord
     // amount of letters in chosen word 
     // let amountOfLetters = chosenWord.length;
     // make an array in which to fetch the answers from
@@ -28,7 +27,32 @@ app.get('/word', (req,res) => {
     // send array
     res.send(word);
 })
+app.get('/word/:word', (req,res)=> { 
+  let chosenWordFromuser = req.params.word
+  console.log(typeof chosenWordFromuser)
+  let chosenWord = genre(chosenWordFromuser)
+  console.log(chosenWord)
+ // ...do your logic with the word here
+ let word = chosenWord.split('');
+  // send it back to the front-end
+  res.send(word)
+})
+let finalWord;
+function genre(chosenGenre) {
+  console.log(chosenGenre)
+  let halloween = ["Spöke", "Ghost", "fotboll"];
+  let sport = ["Fotboll", "Basket", "fotboll"];
+  let tv = ['tre','fyra','fem'];
 
+  if(chosenGenre === 'halloween') {
+     finalWord = halloween[Math.floor(Math.random() * halloween.length)]
+  } else if(chosenGenre === 'sport') {
+    finalWord  =  sport[Math.floor(Math.random() * sport.length)];
+  } else if(chosenGenre === 'tv') {
+    finalWord  = tv[Math.floor(Math.random() * tv.length)];
+  }
+return finalWord
+}
 app.listen(3000, () => {
     console.log('port 3000 up and running')
   })
